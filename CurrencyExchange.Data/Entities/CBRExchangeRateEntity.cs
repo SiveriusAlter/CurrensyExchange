231f653d41984db1f19294d4cc90507c04ae6697
Alexander Zhernakov <neongenesisdatabase@gmail.com>
2025-05-31 16:30:45 +0500

    Fix CBR rate

--- CurrencyExchange.Data/Entities/CBRExchangeRateEntity.cs
+++ CurrencyExchange.Data/Entities/CBRExchangeRateEntity.cs
@@ -2,8 +2,8 @@ namespace CurrencyExchange.Data.Entities;
 
 public class CBRExchangeRateEntity(string code, string name, float rate)
 {
-    public string BaseCurrencyCode { get; } = "RUB";
-    public string TargetCurrencyCode { get; set; } = code;
+    public string BaseCurrencyCode { get; } =  code;
+    public string TargetCurrencyCode { get; set; } = "RUB";
     public string TargetCurrencyName { get; set; } = name;
     public float Rate { get; set; } = rate;
 }
